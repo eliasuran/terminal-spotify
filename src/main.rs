0a2d980de4b572523ae05f9c3ece6588a4d7bdf9
Elias Rissanen Uran <elura001@osloskolen.no>
2024-03-06 14:29:33 +0100

    fixed auth (dont need to parse code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use dotenv::dotenv;
 use rspotify::{prelude::*, scopes, AuthCodeSpotify, Credentials, OAuth};
+use std::{thread, time::Duration};
 use terminal_spotify::get_env;
+
+struct CurrentlyPlaying {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -10,7 +15,21 @@ async fn main() {
     let client_secret = get_env("RSPOTIFY_CLIENT_SECRET");
 
     // user authentication
-    let creds = Credentials::new(&client_id, &client_secret);
+    // returns a variable which all functions used to get data can be run on
+    let spotify = authorize_user(&client_id, &client_secret).await;
+
+    let currently_playing = spotify.current_user_playing_item().await.unwrap();
+    println!("{:?}", currently_playing);
+
+    loop {
+        println!("bruh bruh");
+        thread::sleep(Duration::new(2, 0))
+    }
+}
+
+async fn authorize_user(client_id: &str, client_secret: &str) -> AuthCodeSpotify {
+    let creds = Credentials::new(client_id, client_secret);
+    // localhost:8888 is currently just an express server with one endpoint, callback
     let oauth = OAuth {
         redirect_uri: "http://localhost:8888/callback".to_string(),
         scopes: scopes!(
@@ -20,18 +39,12 @@ async fn main() {
         ),
         ..Default::default()
     };
+
     let spotify = AuthCodeSpotify::new(creds, oauth);
+
     let url = spotify.get_authorize_url(false).unwrap();
     spotify.prompt_for_token(&url).await.unwrap();
 
-    let auth_code = spotify.parse_response_code(&url);
-
-    if auth_code == None {
-        println!("Could not get auth code");
-        return;
-    }
-
-    let converted_auth_code = auth_code.as_deref().unwrap();
-
-    let _access_token = spotify.request_token(converted_auth_code);
+    // no need to parse response code, just return the AuthCodeSpotify
+    return spotify;
 }
